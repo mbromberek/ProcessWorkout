b8031d0dacff82ef91e841341c742847ea274534
Mike Bromberek <mikebromberek@gmail.com>
2019-12-19 07:51:32 -0600

    Setup for scheduling on MacOS Mojave, currently does not work on Catalina I think due to permissions.

---
+++ scheduling/Run_ProcessRunGap.zsh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'Run_ProcessRunGap Start'
+
+echo `whoami`
+
+/usr/bin/osascript /Users/mikeyb/Dropbox/code/python/ProcessWorkout/Scheduling/Run_ProcessRunGap.app
+
+echo 'Run_ProcessRunGap End'
