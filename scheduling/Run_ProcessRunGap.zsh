859d9b8bbcad5604ccd06b96d16b86c3b4adb1a9
Mike Bromberek <mikebromberek@gmail.com>
2019-12-19 12:29:32 -0600

    Delete Run_ProcessRunGap.zsh

--- scheduling/Run_ProcessRunGap.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-echo 'Run_ProcessRunGap Start'
-
-echo `whoami`
-
-/usr/bin/osascript /Users/mikeyb/Dropbox/code/python/ProcessWorkout/scheduling/Run_ProcessRunGap.applescript
-
-echo 'Run_ProcessRunGap End'
