3ccc65bbfc2ebee3bd4cbbb5170d0523d447ac6d
Mike Bromberek <mikebromberek@gmail.com>
2022-05-15 08:22:01 -0500

    getWrkt from server since date

---
+++ ws/getWrkt.py
@@ -0,0 +1,23 @@
+# First party classes
+import os, sys
+import logging
+import logging.config
+from datetime import datetime
+
+# Third party classes
+import configparser
+import requests
+
+# Custom classes
+from ExerciseInfo_Class import ExerciseInfo
+
+def get_wrkt(dttm, wsConfig):
+    logger.info('get_wrkt: ' + str(dttm))
+    dateApiFormat = '%Y-%m-%d'
+    dt_str = datetime.strftime(dttm, dateApiFormat)
+    r = requests.get(wsConfig['server'] + ':' + wsConfig['port'] + '/api/workouts/since/' + dt_str, headers={'Authorization':'Bearer ' + wsConfig['token']}, verify=wsConfig['verifyCert'] == 'Y')
+    if r.status_code == 200:
+        data = r.json()
+        return data
+    else:
+        return None
