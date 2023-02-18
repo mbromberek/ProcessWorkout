a1030d0a296b7d0dec4693abee50f6b891aa9f5d
Mike Bromberek <mikebromberek@gmail.com>
2023-02-18 14:38:20 -0600

    Comment out unused code

--- ProcessRunGap.py
+++ ProcessRunGap.py
@@ -155,16 +155,16 @@ def getWrktWeather(ex, data):
     ex.endWeather = get_weatherkit(ex.endLat, ex.endLon, ex.endTime)
     ex.endWeather.position = 'End'
 
-    if config['weather_details']['log_details'] == 'Y':
-        f = open('/tmp/weatherData' + ex.startTime.strftime('%Y%m') + '.txt', 'a')
-        f.write('Start Time:' + ex.startTime.strftime('%Y-%m-%dT%H:%M:%S') + '\n')
-        f.write('Lat: ' + str(ex.startLat) + ' Lon: ' + str(ex.startLon) + '\n')
-        f.write(ex.startWeather.generateWeatherUserNotes('DarkSkyStart: '))
-        f.write(getWeatherApi(ex.startLat, ex.startLon, roundHour(ex.startTime)).generateWeatherUserNotes('WeatherStart: '))
-        f.write(ex.endWeather.generateWeatherUserNotes('DarkSkyEnd: '))
-        f.write(getWeatherApi(ex.endLat, ex.endLon, roundHour(ex.endTime)).generateWeatherUserNotes('WeatherEnd: '))
-        f.write('End Time:' + ex.startTime.strftime('%Y-%m-%dT%H:%M:%S') + '\n')
-        f.close()
+    # if config['weather_details']['log_details'] == 'Y':
+    #     f = open('/tmp/weatherData' + ex.startTime.strftime('%Y%m') + '.txt', 'a')
+    #     f.write('Start Time:' + ex.startTime.strftime('%Y-%m-%dT%H:%M:%S') + '\n')
+    #     f.write('Lat: ' + str(ex.startLat) + ' Lon: ' + str(ex.startLon) + '\n')
+    #     f.write(ex.startWeather.generateWeatherUserNotes('DarkSkyStart: '))
+    #     f.write(getWeatherApi(ex.startLat, ex.startLon, roundHour(ex.startTime)).generateWeatherUserNotes('WeatherStart: '))
+    #     f.write(ex.endWeather.generateWeatherUserNotes('DarkSkyEnd: '))
+    #     f.write(getWeatherApi(ex.endLat, ex.endLon, roundHour(ex.endTime)).generateWeatherUserNotes('WeatherEnd: '))
+    #     f.write('End Time:' + ex.startTime.strftime('%Y-%m-%dT%H:%M:%S') + '\n')
+    #     f.close()
 
     ex.userNotes = ex.userNotes + ex.startWeather.generateWeatherUserNotes()
     ex.userNotes = ex.userNotes + ex.endWeather.generateWeatherUserNotes()
